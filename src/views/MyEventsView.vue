382921edad1c51ae4190d3b66f44a6175a3eb7ce
jhocoder <jhonathanrugeb@gmail.com>
2025-02-17 22:04:17 +0100

    added function watched

--- src/views/MyEventsView.vue
+++ src/views/MyEventsView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref, watch } from 'vue';
 import { getDocs, collection } from 'firebase/firestore';
 import { db } from '@/firebase/sdk';
 import { onMounted } from 'vue';
@@ -8,7 +8,11 @@ import { getDoc, doc } from 'firebase/firestore';
 
 
 const eventosUsuario = ref(null)
+
+
 const eventos = ref([])
+
+
 onMounted(async () => {
   const usuarioId = auth.currentUser.uid
   
@@ -16,17 +20,31 @@ onMounted(async () => {
   const obtenerDoc = await getDoc(usuarioRef)
   const dataUsuario = obtenerDoc.data()
   eventosUsuario.value = dataUsuario.mis_eventos
-  console.log(dataUsuario)
+  eventos.value = eventosUsuario.value
 
 })
 
 console.log(eventos.value)
 
+const busqueda = ref(null)
+
+watch(busqueda, (nuevoValor) => {
+  if (!nuevoValor) {
+    eventos.value = [...eventosUsuario.value];
+  } else {
+    eventos.value = eventosUsuario.value.filter((evento) =>
+      evento.nombre.toLowerCase().includes(nuevoValor.toLowerCase())
+    );
+  }
+});
+
+
 </script>
 
 <template>
     <h1>My Events</h1>
-    <ul v-for="evento in eventosUsuario" :key="evento.nombre">
+    <input type="text" placeholder="Buca tu evento Guardado" v-model="busqueda">
+    <ul v-for="evento in eventos" :key="evento.nombre">
         <li>
           Nombre:  {{ evento.nombre }}
           Descripcion:  {{ evento.descripcion }}
